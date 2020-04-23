afb24a4221235a221951370c50d8bcdc896b33fd
Lee Raulin <leeraulin@gmail.com>
2020-04-23 13:33:21 -0400

    Set up routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,35 @@
+import { BorrowerComponent } from './borrower/borrower/borrower.component';
+import { AdminComponent } from './admin/admin/admin.component';
+import { LibrarianComponent } from './librarian/librarian/librarian.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    HomeComponent,
+    PageNotFoundComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      { path: 'admin', component: AdminComponent },
+      { path: 'borrower', component: BorrowerComponent },
+      { path: 'librarian', component: LibrarianComponent },
+      { path: '', component: HomeComponent, pathMatch: 'full' },
+      { path: '**', component: PageNotFoundComponent },
+    ]),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
