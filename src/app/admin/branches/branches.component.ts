a3e0724e7e4a3330e21220e08e48eb16c0280c51
Lee Raulin <leeraulin@gmail.com>
2020-04-27 14:03:27 -0400

    add branch table

--- src/app/admin/branches/branches.component.ts
+++ src/app/admin/branches/branches.component.ts
@@ -1,15 +1,79 @@
-import { Component, OnInit } from '@angular/core';
+import { DialogBoxComponent } from './../dialog-box/dialog-box.component';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatTable } from '@angular/material/table';
+import { MatDialog } from '@angular/material/dialog';
+
+interface Branch {
+  id: number;
+  name: string;
+  address: string;
+}
+
+const BRANCHES: Branch[] = [
+  {
+    id: 1,
+    name: 'Library of Congress',
+    address: 'Independence Ave SE, Washington, DC 20540',
+  },
+  {
+    id: 2,
+    name: 'Miami-Dade Library',
+    address: '101 W Flagler St, Miami, FL 33130',
+  },
+];
 
 @Component({
   selector: 'app-branches',
   templateUrl: './branches.component.html',
-  styleUrls: ['./branches.component.css']
+  styleUrls: ['./branches.component.css'],
 })
-export class BranchesComponent implements OnInit {
+export class BranchesComponent {
+  displayedColumns: string[] = ['id', 'name', 'address'];
+  dataSource = BRANCHES;
 
-  constructor() { }
+  @ViewChild(MatTable, { static: true }) table: MatTable<any>;
 
-  ngOnInit(): void {
+  constructor(public dialog: MatDialog) {}
+  openDialog(action, obj) {
+    obj.action = action;
+    const dialogRef = this.dialog.open(DialogBoxComponent, {
+      width: '250px',
+      data: obj,
+    });
+
+    dialogRef.afterClosed().subscribe((result) => {
+      if (result.event === 'Add') {
+        this.addRowData(result.data);
+      } else if (result.event === 'Update') {
+        this.updateRowData(result.data);
+      } else if (result.event === 'Delete') {
+        this.deleteRowData(result.data);
+      }
+    });
   }
 
+  addRowData(rowObj) {
+    const d = new Date();
+    this.dataSource.push({
+      id: d.getTime(),
+      name: rowObj.name,
+      address: rowObj.address,
+    });
+    this.table.renderRows();
+  }
+  updateRowData(rowObj) {
+    this.dataSource = this.dataSource.filter((value, key) => {
+      if (value.id === rowObj.id) {
+        value.id = rowObj.id;
+        value.name = rowObj.name;
+        value.address = rowObj.address;
+      }
+      return true;
+    });
+  }
+  deleteRowData(rowObj) {
+    this.dataSource = this.dataSource.filter((value, key) => {
+      return value.id !== rowObj.id;
+    });
+  }
 }
