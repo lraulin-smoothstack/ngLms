115cc027b712533cb14bf90654bf0f8e7f5e0217
Lee Raulin <leeraulin@gmail.com>
2020-05-03 14:40:31 -0400

    ...

--- src/app/admin/books/books.component.ts
+++ src/app/admin/books/books.component.ts
@@ -1,8 +1,10 @@
+import { Author } from './../types';
 import { AdminService } from './../admin.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChildren, QueryList } from '@angular/core';
 import { Book } from '../types';
 import { NgbModal, NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
 import { Pager, PagerService } from 'src/app/common/services/pager.service';
+import { SortableDirective, SortEvent } from '../sortable.directive';
 
 const newBook = (): Book => ({
   id: null,
@@ -27,12 +29,45 @@ export class BooksComponent implements OnInit {
   pager: Pager;
   pagedItems: Book[];
   itemsPerPage = 5;
+  arrows = { title: '', author: '', publisher: '', genre: '' };
+
+  @ViewChildren(SortableDirective) headers: QueryList<SortableDirective>;
 
   constructor(
     private adminService: AdminService,
     private modalService: NgbModal,
     private pagerService: PagerService
   ) {}
+
+  onSort({ column, direction }: SortEvent) {
+    console.log('Sorting...');
+    // resetting other headers
+    this.headers.forEach((header) => {
+      if (header.sortable !== column) {
+        header.direction = '';
+      }
+    });
+
+    this.books = this.sort(this.books, column, direction);
+    this.arrows[column] =
+      direction === 'asc' ? '△' : direction === 'desc' ? '▽' : '';
+    this.setPage(this.pager.currentPage);
+  }
+
+  compare(v1, v2) {
+    return v1 < v2 ? -1 : v1 > v2 ? 1 : 0;
+  }
+
+  sort(items: Book[], column: string, direction: string): Book[] {
+    if (direction === '') {
+      return items;
+    } else {
+      return [...items].sort((a, b) => {
+        const res = this.compare(a[column], b[column]);
+        return direction === 'asc' ? res : -res;
+      });
+    }
+  }
 
   fetchData(): void {
     this.adminService.getBooks().subscribe({
