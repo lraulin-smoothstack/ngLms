1f56ed4c11762c28dacb4e5e5a3ea7900b8b9728
Lee Raulin <leeraulin@gmail.com>
2020-05-28 13:43:19 -0400

    delete old test

--- src/app/admin/authors/authors.component.spec.ts
+++ src/app/admin/authors/authors.component.spec.ts
@@ -86,15 +86,6 @@ fdescribe('AuthorsComponent', () => {
   });
 
   describe('open', () => {
-    // it('should open modal with correct title ("Add Author") when called without Author object', () => {
-    //   expect(component.editAuthorModalRef).toBeTruthy();
-    //   component.open(component.editAuthorModalRef);
-    //   fixture.detectChanges();
-    //   const headerText = fixture.debugElement.queryAll(By.css('h5'))[0]
-    //     .nativeElement.innerText;
-    //   expect(headerText).toBeTruthy();
-    // });
-
     it('should be called with one argument when user clicks on Add button', fakeAsync(() => {
       const openSpy = spyOn(component, 'open');
 
