45ff180407c4ca8f1e1a22e59e1b758380dd2526
timothy-breitenfeldt <timothy.breitenfeldt@smoothstack.com>
2020-06-03 14:44:46 -0400

    Implemented login code, still troubleshooting cause for data to not load.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
@@ -9,6 +9,11 @@ import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { PagerService } from './common/services/pager.service';
+import { LoginComponent } from './login/login.component';
+import { AuthenticationService } from './common/services/authentication.service';
+import { JwtInterceptor } from './interceptors/jwt.interceptor';
+import { ErrorInterceptor } from './interceptors/error.interceptor';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -16,17 +21,22 @@ import { PagerService } from './common/services/pager.service';
     HeaderComponent,
     HomeComponent,
     PageNotFoundComponent,
+    LoginComponent,
   ],
   imports: [
     BrowserAnimationsModule,
     AppRoutingModule,
     HttpClientModule,
-    NgbModule
+    NgbModule,
+    FormsModule,
   ],
   providers: [
     PagerService,
+    AuthenticationService,
+    { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
     { provide: 'domain', useValue: 'http://localhost:8080' },
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule {}
---
+++ src/app/common/services/authentication.service.ts
@@ -0,0 +1,43 @@
+﻿import { Injectable, Inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
+
+import { User } from '../interfaces/user.interface';
+
+@Injectable({ providedIn: 'root' })
+export class AuthenticationService {
+  private currentUserSubject: BehaviorSubject<User>;
+  public currentUser: Observable<User>;
+
+  constructor(
+    private http: HttpClient,
+    @Inject('domain') private domain: string
+  ) {
+    this.currentUserSubject = new BehaviorSubject<User>(
+      JSON.parse(localStorage.getItem('currentUser'))
+    );
+    this.currentUser = this.currentUserSubject.asObservable();
+  }
+
+  public get currentUserValue(): User {
+    return this.currentUserSubject.value;
+  }
+
+  login(credentials: Object): Observable<User> {
+    return this.http
+      .post<User>(`${this.domain}/lms/authenticate`, credentials)
+      .pipe(
+        tap((user: User) => {
+          sessionStorage.setItem('currentUser', JSON.stringify(user));
+          this.currentUserSubject.next(user);
+        })
+      );
+  }
+
+  logout() {
+    // remove user from local storage and set current user to null
+    sessionStorage.removeItem('currentUser');
+    this.currentUserSubject.next(null);
+  }
+}
